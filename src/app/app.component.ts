35adf91db227becf7a0f09528f2a37f1a2dbbe5c
Pujan Shakya <i213109@d2hs.com>
2019-05-31 19:34:37 +0545

    Committed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  loadedFeature = '';
+
+  onNavigate(feature: string)
+  {
+    this.loadedFeature = feature;
+  }
+
 }
+ 
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Output , EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
@@ -7,15 +7,23 @@ import { Recipe } from '../recipe.model'
 })
 export class RecipeListComponent implements OnInit {
  
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe 1','This is simply a test',
+    new Recipe('Spring Roll','This is simply a test',
     'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'),
-    new Recipe('A Test Recipe 2','This is simply a test',
+    new Recipe('Pizza','This is simply a test',
     'https://images.pexels.com/photos/208537/pexels-photo-208537.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940')
   ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe)
+  {
+  
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
